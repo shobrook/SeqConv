6a265ae51dabd60451edba3588c712da735bf42b
shobrook <shobrookj@gmail.com>
2020-07-22 23:17:03 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from codecs import open
+
+if sys.version_info[:3] < (3, 0, 0):
+    print("Requires Python 3 to run.")
+    sys.exit(1)
+
+with open("README.md", encoding="utf-8") as file:
+    readme = file.read()
+
+setup(
+    name="lstm-conv",
+    description="Graph convolutional operator that uses a LSTM as a filter",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    version="v1.0.0",
+    packages=["lstm-conv"],
+    python_requires=">=3",
+    url="https://github.com/shobrook/lstm-conv",
+    author="shobrook",
+    author_email="shobrookj@gmail.com",
+    # classifiers=[],
+    install_requires=["torch", "torch_geometric"],
+    keywords=[
+        "gnn",
+        "graph-neural-network",
+        "convolution",
+        "pytorch",
+        "graph",
+        "lstm",
+        "rnn",
+        "recurrent"
+    ],
+    license="MIT"
+)
